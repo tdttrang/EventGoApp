97df65a32f111a0d723fde2ee54bc1f89e31cc0a
tdttrang <duongtrangthuy147012@gmail.com>
2025-05-31 03:19:10 +0700

    api

--- configs/Apis.js
+++ configs/Apis.js
@@ -5,6 +5,11 @@ const BASE_URL = "https://mynameisgiao.pythonanywhere.com/";
 export const endpoints = {
   login: "/o/token/",
   currentUser: "/current-user/profile/",
+  // Thêm các endpoints mới cho đánh giá và phản hồi
+    "create-event-review": (eventId) => `/events/${eventId}/reviews/`,
+  "event-review-list": (eventId) => `/events/${eventId}/reviews/list/`,
+  "create-review-reply": (reviewId) => `events/reviews/${reviewId}/reply/`,
+  "review-reply-list": (reviewId) => `/events/reviews/${reviewId}/replies/`,
 };
 
 export const authApi = (accessToken) =>
