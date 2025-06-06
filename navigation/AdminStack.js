78d04159a4528b5e5f40916b945dff3ff2ac217f
MynameisGiao <2004phuonggiao@gmail.com>
2025-06-06 14:37:46 +0700

    resetStack

--- navigation/AdminStack.js
+++ navigation/AdminStack.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import AdminUserManager from "../components/Admin/AdminUserManager";
+import AdminDashBoard from "../components/Admin/AdminDashboard";
 import AdminEventManager from "../components/Admin/AdminEventManager";
 import AdminStats from "../components/Admin/AdminStats";  
-import AdminDashboard from "../components/Admin/AdminDashboard";  
 
 const Stack = createNativeStackNavigator();
 
 const AdminStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
-      <Stack.Screen name="AdminUserManager" component={AdminUserManager} />
+      <Stack.Screen name="AdminDashBoard" component={AdminDashBoard} />
       <Stack.Screen name="AdminEventManager" component={AdminEventManager} />
       <Stack.Screen name="AdminStats" component={AdminStats} />
     </Stack.Navigator>
